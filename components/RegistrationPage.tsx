95a91117ee283b960c6b5fa80067781bb2f8ffe0
Crys Garage Developer <developer@crysgarage.com>
2025-08-20 14:25:00 +0100

    Fix duplicate header on Join Us page by removing local Navigation/Footer from RegistrationPage and relying on global layout

--- components/RegistrationPage.tsx
+++ components/RegistrationPage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { SocialMediaRegistrationForm } from './SocialMediaRegistrationForm';
-import { Navigation } from './Navigation';
-import { Footer } from './Footer';
 
 interface RegistrationPageProps {
   onPageChange: (page: string) => void;
@@ -10,11 +8,9 @@ interface RegistrationPageProps {
 export function RegistrationPage({ onPageChange }: RegistrationPageProps) {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navigation currentPage="register" onPageChange={onPageChange} />
       <main className="flex-1">
         <SocialMediaRegistrationForm showHeader={false} />
       </main>
-      <Footer onPageChange={onPageChange} />
     </div>
   );
 }
